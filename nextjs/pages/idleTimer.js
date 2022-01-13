af6be76416306bfc7c26cf0d9b3942272f6fc152
MartinRenteria <renteriamp96@gmail.com>
2022-01-13 08:39:32 -0800

    fixed the mouse movement

--- nextjs/pages/idleTimer.js
+++ nextjs/pages/idleTimer.js
@@ -2,14 +2,17 @@
 let idleTime = 0;
 // increment the idle time counter every second
 let idleInterval = setInterval(timerIncrement, 1000); // 1 second
-
-function timerIncrement() {
+function timerIncrement(e) {
     idleTime = idleTime + 1;
 
+    if (idleTime === 20) {
+        alert('Still have questions? Book a time to speak to our team and find the right solution."')
+    }
     // console.log(idleTime)
 }
 
-function clearTimer({idleTime, idleInterval}) {
+// clears the timer and sets the idletimer to zero
+function clearTimer() {
     idleTime = 0;
 }
 
