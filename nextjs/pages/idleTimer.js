c909e5173be429593e5c22fd6efaef9fd17b68d4
MartinRenteria <renteriamp96@gmail.com>
2022-01-14 09:54:42 -0800

    Refactored idletimer using usestate and useEffect hooks

--- nextjs/pages/idleTimer.js
+++ nextjs/pages/idleTimer.js
@@ -1,29 +1,31 @@
-// Starting idleTime
-let idleTime = 0;
-// increment the idle time counter every second
-let idleInterval = setInterval(timerIncrement, 1000); // 1 second
-function timerIncrement(e) {
-    idleTime = idleTime + 1;
-
-    if (idleTime === 20) {
-        alert('Still have questions? Book a time to speak to our team and find the right solution."')
-    }
-    // console.log(idleTime)
-}
-
-// clears the timer and sets the idletimer to zero
-function clearTimer() {
-    idleTime = 0;
-}
+import { useState, useEffect } from "react";
 
 const idleTimer = () => {
+    const [idleTime, timer] = useState(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          timer(idleTime => idleTime + 1)
+        }, 1000);
+      }, []);
+
+      if(idleTime === 10) {
+        timer(idleTime = 0);
+        alert("Are you still there?")
+    }; 
+    
+// clears the timer and sets the idletimer to zero
+const clearTimer = () => {
+    timer(idleTime = 0)
+}
+
     return (
     <div onMouseMove={clearTimer}>
     <button><a href='/'>Home</a></button>
     <h1>A simple idle timer</h1>
     <p className="timertext">
         You are idle for
-        <span> </span>  
+        <span> {idleTime} </span>  
         </p>
   
     </div>
